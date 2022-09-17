c0cf5e1a45280426b1472197744584d873c3cc68
Parker Collier <pcollie4@vols.utk.edu>
2022-09-16 21:45:09 -0400

    encryption

--- aes.cpp
+++ aes.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <iomanip>
 
+//for our standard Nb = 4
+const unsigned int Nb = 4;
+
+//num rounds and key size
+unsigned int Nr = 10;
+unsigned int Nk = 4;
+
+
+//lookup table for sbox
 unsigned char box[256] =
 {
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -20,7 +30,28 @@ unsigned char box[256] =
     0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
 };
 
+//rcon lookup table
+unsigned char rcon[256] = {
+    0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
+    0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39,
+    0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a,
+    0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8,
+    0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef,
+    0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc,
+    0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b,
+    0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3,
+    0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94,
+    0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
+    0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35,
+    0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f,
+    0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04,
+    0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63,
+    0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd,
+    0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d
+};
 
+
+//helper function I made to print every bit in a byte
 void bitcheck(unsigned char test){
     for (int i = 7; i >=0 ; i--){
         std::cout << ((test >> i) & 0x01);
@@ -28,14 +59,55 @@ void bitcheck(unsigned char test){
     std::cout << std::endl;
 }
 
+//helper function for checking hex value of bytes
+void bigcheck(unsigned char* test, int len){
+    for(int i = 0; i < len; i++){
+        std::cout << std::hex << static_cast<unsigned int>(test[i]) << " ";
+    }
+    std::cout << std::endl;
+}
+
+void statecheck(unsigned char state[4][Nb]){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            std::cout << std::hex << static_cast<unsigned int>(state[i][j]) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void print(unsigned char state[4][Nb], const char* phrase, int r){
+
+    std::cout  << "round[" << r << "]" << phrase;
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            std::cout<< std::hex << static_cast<unsigned int>(state[j][i]) << " ";
+        }
+    }
+
+    std::cout << std::endl;
+}
+
+void printline(unsigned char* test, int len, const char* phrase, int r){
+    std::cout  << "round[" << r << "]" << phrase;
+    for(int i = 0; i < len; i++){
+        std::cout << std::hex << static_cast<unsigned int>(test[i]) << " ";
+    }
+    std::cout << std::endl;
+}
+
+//add two finite fields
 unsigned char ffadd(unsigned char a, unsigned char b){
     return a ^ b;
 }
 
+//muiltiply a finit field by 1
 unsigned char xtime(unsigned char x){
     return (x << 1) ^ (x & 0x80 ? 0x1b : 0);
 }
 
+//multiply two finite fields using method demonstrated in class
 unsigned char ffmultiply(unsigned char a, unsigned char b){
     unsigned char sum = 0;
     
@@ -50,12 +122,14 @@ unsigned char ffmultiply(unsigned char a, unsigned char b){
     return sum;
 }
 
+//sub all bytes in a word with appropriate sbox values
 void subWord(unsigned char* x){
     for(int i = 0; i < 4; i++){
         x[i] = box[x[i]];
     }
 }
 
+//cyclic permutation of a word
 void rotWord(unsigned char* x){
     unsigned char tmp = x[0];
     for (int i = 0; i < 4; i++){
@@ -68,8 +142,166 @@ void rotWord(unsigned char* x){
     }
 }
 
-int main(){
-    unsigned char test[] = {0x11, 0x22, 0x33, 0x44};
-    rotWord(test);
-    bitcheck(test[3]);
+void KeyExpansion(const unsigned char* key, unsigned char* w){
+    unsigned char temp[4];
+
+    unsigned int i = 0;
+    while(i < 4 * Nk){
+        w[i * 4] = key[i * 4];
+        w[i * 4 + 1] = key[i * 4 + 1];
+        w[i * 4 + 2] = key[i * 4 + 2];
+        w[i * 4 + 3] = key[i * 4 + 3];
+        i++;
+    }
+
+    i = Nk;
+
+    while(i < Nb * (Nr+1)){
+        for (int j = 0; j < 4; j++){
+            temp[j] = w[(i-1) * 4 + j];
+        }
+
+        if (i % Nk == 0){
+            rotWord(temp);
+            subWord(temp);
+
+            temp[0] = temp[0] ^ rcon[i/Nk];
+        }
+        else if (Nk > 6 && i % Nk == 4){
+            subWord(temp);
+        }
+
+        w[i * 4 + 0] = w[(i - Nk) * 4 + 0] ^ temp[0];
+        w[i * 4 + 1] = w[(i - Nk) * 4 + 1] ^ temp[1];
+        w[i * 4 + 2] = w[(i - Nk) * 4 + 2] ^ temp[2];
+        w[i * 4 + 3] = w[(i - Nk) * 4 + 3] ^ temp[3];
+
+        i++;
+    }
 }
+
+void subBytes(unsigned char state[4][Nb]){
+    for (unsigned int i = 0; i < 4; i++){
+        for (unsigned int j = 0; j < Nb; j++){
+            state[i][j] = box[state[i][j]];
+        }
+    }
+}
+
+void shiftRows(unsigned char state[4][Nb]){
+    //first row stays the same
+    
+    //second row rotates by 1
+    unsigned char tmp;
+    tmp = state[1][0];
+    state[1][0] = state[1][1];
+    state[1][1] = state[1][2];
+    state[1][2] = state[1][3];
+    state[1][3] = tmp;
+
+    //third row rotates by 2
+    tmp = state[2][0];
+    state[2][0] = state[2][2];
+    state[2][2] = tmp;
+    
+    tmp = state[2][1];
+    state[2][1] = state[2][3];
+    state[2][3] = tmp;
+
+    //fourth row roates by 3
+    tmp = state[3][0];
+    state[3][0] = state[3][3];
+    state[3][3] = state[3][2];
+    state[3][2] = state[3][1];
+    state[3][1] = tmp;
+}
+
+void MixColumns(unsigned char state[4][Nb]){
+    
+    unsigned char tmp[4][Nb];
+    for (unsigned int i = 0; i < Nb; i++){
+        tmp[0][i] = (xtime(state[0][i]) ^ (xtime(state[1][i]) ^ state[1][i]) ^ state[2][i] ^ state[3][i]);
+        tmp[1][i] = (state[0][i] ^ xtime(state[1][i]) ^ (xtime(state[2][i]) ^ state[2][i]) ^ state[3][i]);
+        tmp[2][i] = (state[0][i] ^ state[1][i] ^ xtime(state[2][i]) ^ (xtime(state[3][i]) ^ state[3][i]));
+        tmp[3][i] = ((xtime(state[0][i]) ^ state[0][i]) ^ state[1][i] ^ state[2][i] ^ xtime(state[3][i]));
+    }
+
+    for (unsigned int i = 0; i < Nb; i++){
+        for(unsigned int j = 0; j < 4; j++){
+            state[i][j] = tmp[i][j];
+        }
+    }
+
+}
+
+void addRoundKey(unsigned char state[0][Nb], unsigned char* w){
+    for (unsigned int i = 0; i < Nb; i++){
+        for (unsigned int j = 0; j < 4; j++){
+            state[j][i] ^= w[i * 4 + j];
+        }
+    }
+}
+
+void Cipher(unsigned char in[4*Nb], unsigned char out[4*Nb], unsigned char* w){
+    unsigned char state[4][Nb];
+    
+    for(unsigned int i = 0; i < Nb; i++){
+        for (unsigned int j = 0; j < 4; j++){
+            state[j][i] = in[i * 4 + j];
+        }
+    }
+
+    printline(in, 16, ".input     ", 0);
+    printline(w, 16, ".k_sch     ", 0);
+
+    addRoundKey(state, w);
+
+
+    for (unsigned int i = 1; i < Nr; i++){
+        print(state, ".start     ", i);
+        subBytes(state);
+        print(state, ".s_box     ", i);
+        shiftRows(state);
+        print(state,".s_row     ", i);
+        MixColumns(state);
+        print(state,".m_col     ",i);
+
+        addRoundKey(state, w + (i * 4 * Nb));
+        printline(w + (i * 4 * Nb), 16, ".k_sch     ", i);
+    }
+
+    subBytes(state);
+    print(state, ".s_box     ", Nr);
+    shiftRows(state);
+    print(state,".s_row     ", Nr);
+
+    addRoundKey(state, w + (Nr * 4 * Nb));
+    printline(w + (Nr * 4 * Nb), 16, ".k_sch     ", Nr);
+
+    for(unsigned int i = 0; i < Nb; i++){
+        for (unsigned int j = 0; j < 4; j++){
+            out[i * 4 + j] = state[j][i];
+        }
+    }
+
+    printline(out, 16, ".output    ", Nr);
+
+
+}
+
+int main(){
+
+    unsigned char input[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    unsigned char output[16];
+
+    int klen = 4 * Nb * (Nr + 1);
+    unsigned char w [klen];
+    unsigned char Key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+
+    KeyExpansion(Key, w);
+
+    Cipher(input, output, w);
+
+
+}
+
